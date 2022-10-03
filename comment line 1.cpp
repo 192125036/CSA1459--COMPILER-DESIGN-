e61c8cc33e63a98bf953461a0db90a565636b587
192125036 <113360834+192125036@users.noreply.github.com>
2022-10-03 10:59:59 +0530

    Checking whether the input is comment or not..

---
+++ comment line 1.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	char com [30];
+	int i=2,a=0;
+	
+	printf("\n Enter the line or Text : ");
+	gets(com);
+	if(com[0]=='/')
+	{
+	if(com[1]=='/')
+	printf("\n It is a Comment.");
+	else if(com [1]=='*')
+	{
+	for(i=2;i<=30;i++)
+	{
+	if(com[i]=='*'&&com[i+1]=='/')
+	{
+	printf("\n It is a Comment.");
+	a=1;
+	break;
+	}
+	else 
+	continue;
+	}
+	if(a==0)
+	printf("\n It is Not a Comment.");
+	}
+	else
+	printf("\n It is Not a Comment.");
+	}
+	else
+	printf("\n It is Not a Comment.");
+	}
